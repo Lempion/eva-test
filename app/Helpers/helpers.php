7d3ca0e962ac8594eb0bbf1f6c2263fb3fa58d23
Vitaly <vit.fedor2012@mail.ru>
2023-01-25 12:50:01 +0300

    * Вынес повторяющиеся функции в отдельный файл helpers * Зарефакторил код и подправил MainFormController.php

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!function_exists('countCharsLangInStr')) {
+    function countCharsLangInStr($str, $lang = 'ru')
+    {
+        $languages = ['ru' => 'а-яё', 'en' => 'a-z'];
+
+        if (empty($languages[$lang]) && empty($str)) {
+            return false;
+        }
+
+        preg_match_all('/[' . $languages[$lang] . ']/ui', $str, $matches);
+
+        return count($matches[0]);
+    }
+}
+
+if (!function_exists('detectLangAndReplace')) {
+    function detectLangAndReplace($ruChars, $enChars, $str): array
+    {
+        $languages = ['ru' => 'а-яё', 'en' => 'a-z'];
+
+        $currentLang = ($ruChars >= $enChars ? 'en' : 'ru');
+
+        $text = preg_replace('/([' . $languages[$currentLang] . '])/ui', '<strong>\1</strong>', $str);
+
+        return ['lang' => $currentLang, 'text' => $text];
+    }
+
+}
